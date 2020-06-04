c1824c78cbf5ca340c7dd81c5a43ffc7904890dc
Futigami <erick.futigami@fatec.sp.gov.br>
2020-06-04 14:05:11 -0300

    Aula 4 v2

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const server = express()
+
+//Configurar pasta publica
+server.use(express.static("public"))
+
+
+//Utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+//Configurar caminhos da aplicação
+//Página inicial
+// req: Requisição
+// res: Resposta
+server.get("/", (req, res) => {
+    return res.render("index.html", { title: "Um título" })
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
+
+//Ligar o servidor
+server.listen(3000)
